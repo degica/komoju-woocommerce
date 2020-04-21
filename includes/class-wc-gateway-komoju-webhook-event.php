b4658f19a0dcc3600b3013af46180de442960487
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-21 17:32:47 +1000

    Return 400 on JSON decode failure
    If the plugin can't decode the JSON body of the webhook event then it's
    a problem with the request so a 400 status code is more appropriate and
    more informative than a 500 in this case.

--- includes/class-wc-gateway-komoju-webhook-event.php
+++ includes/class-wc-gateway-komoju-webhook-event.php
@@ -21,7 +21,7 @@ class WC_Gateway_Komoju_Webhook_Event {
 
         if (! empty( json_last_error() )) {
             $errorMsg = "Komoju IPN Request JSON Decoding Failure. Error: " . json_last_error_msg();
-            wp_die( $errorMsg, "Komoju IPN", array( 'response' => 500 ) );
+            wp_die( $errorMsg, "Komoju IPN", array( 'response' => 400 ) );
         }
     }
 
