8b3de646a0886fcde3e421df065271908bef8a79
Dinwy <whix2010@gmail.com>
2024-05-08 15:10:39 +0900

    Clean up and show icons

--- includes/js/komoju-checkout-blocks.js
+++ includes/js/komoju-checkout-blocks.js
@@ -1,8 +1,30 @@
+const { useEffect, useCallback, useRef, createElement } = window.wp.element;
+
 function registerPaymentMethod(paymentMethod) {
     let name = `${paymentMethod.id}`
 
     const settings = window.wc.wcSettings.getSetting(`${name}_data`, {});
-    const label = window.wp.htmlEntities.decodeEntities(settings.title) || window.wp.i18n.__('NULL GATEWAY', 'test_komoju_gateway');
+
+    const komojuField = createElement('komoju-fields', {
+        'name': 'komoju_payment_token',
+        'komoju-api': settings.komojuApi,
+        'publishable-key': settings.publishableKey,
+        'session': settings.session,
+        'payment-type': settings.paymentType,
+        style: { display: 'none' },
+    });
+
+    const label = createElement('div', {
+        style: { display: 'block', alignItems: 'center', justifyContent: 'center' }
+    },
+        window.wp.htmlEntities.decodeEntities(settings.title || window.wp.i18n.__('NULL GATEWAY', 'test_komoju_gateway')),
+        createElement('img', {
+            src: settings.icon,
+            alt: settings.title || 'Payment Method Icon',
+            style: { display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: '10px' }
+        }),
+        komojuField,
+    );
     const Content = () => {
         return window.wp.htmlEntities.decodeEntities(settings.description || '');
     };
@@ -10,10 +32,10 @@ function registerPaymentMethod(paymentMethod) {
     const Block_Gateway = {
         name: name,
         label: label,
-        content: Object(window.wp.element.createElement)(Content, null),
-        edit: Object(window.wp.element.createElement)(Content, null),
+        content: createElement(Content, null),
+        edit: createElement(Content, null),
         canMakePayment: () => true,
-        ariaLabel: label,
+        ariaLabel: settings.title || 'Payment Method',
         supports: {
             features: settings.supports || ['products'],
         },
