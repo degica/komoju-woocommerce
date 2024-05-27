82928b600dc931802e5c0c1ef1f0dd43df8deb27
Dinwy <whix2010@gmail.com>
2024-05-27 14:22:26 +0900

    Hide invisible fields

--- includes/js/komoju-checkout-blocks.js
+++ includes/js/komoju-checkout-blocks.js
@@ -46,7 +46,7 @@ const KomojuPaymentModule = (() => {
                 if (paymentMethod.id != activePaymentMethod) return;
 
                 const komojuField = document.querySelector(`komoju-fields[payment-type='${paymentMethod.paymentType}']`);
-                komojuField.style.display = 'block';
+                komojuField.style.display = komojuFieldEnabledMethods.includes(paymentMethod.id) ? 'block' : 'none';
                 const descriptionElement = document.getElementById(`${name}_description`);
                 descriptionElement.style.display = 'block';
 
