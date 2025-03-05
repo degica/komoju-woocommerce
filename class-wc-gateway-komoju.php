42640b92cddce7bd73cac9ef093f6472a562ed95
Jae <kmin@degica.com>
2025-03-05 15:40:49 +0900

    Bump version to 3.1.9

--- class-wc-gateway-komoju.php
+++ class-wc-gateway-komoju.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
  *
  * @extends     WC_Payment_Gateway
  *
- * @version     3.1.8
+ * @version     3.1.9
  *
  * @author      Komoju
  */
--- class-wc-settings-page-komoju.php
+++ class-wc-settings-page-komoju.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
  *
  * @extends     WC_Settings_Page
  *
- * @version     3.1.8
+ * @version     3.1.9
  *
  * @author      Komoju
  */
